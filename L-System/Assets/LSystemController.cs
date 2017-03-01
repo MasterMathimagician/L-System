648e55664b865cf0990db0b4eda9f9ae251c734b
Stephen Trotter <trotters@uvic.ca>
2017-03-01 13:47:14 -0800

    basic setup done

---
+++ L-System/Assets/LSystemController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LSystemController : MonoBehaviour {
+
+	//public List<string> rule_in = {""};
+	//public List<string> rule_out = {""};
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	string String_Replace(string input){
+		string output = input;
+
+
+		return output;
+	}
+
+}
